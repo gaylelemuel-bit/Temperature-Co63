3c2dcf6977681981c093eb268d466f30a77e7fd2
gaylelemuel.bit <gaylelemuel@gmail.com>
2025-11-02 00:50:25 -0500

    Assignment1

---
+++ Scripts/Assignment1.js
@@ -0,0 +1,19 @@
+// Funny Story 1
+let myCharacter="Rj";
+let myPlace="Dallas";
+let myObject="A gaint snake";
+let action="ran over it and peeped himself";
+let time="late night";
+const road="I-35";
+console.log( "This may not be true")
+console.log("Once Upon a time lol")
+console.log("When "+myCharacter+" was rding around on " + myPlace+" texas roads " + time + " in his car. " +myObject+" flew off a pickup truck in front of us and he "+action+ " on "+ road);
+
+// Funny Story 2
+myCharacter="Lj";
+myPlace="dallas";
+myObject="spaceship";
+action="flying/driving";
+time="afternoon";
+console.log( "Another not so true story.. maybe lol")
+console.log("When "+myCharacter+ " was " +action+ " around in his luxury " + myObject+ " in the late " + time + " he just saw Rj car." +road+ "seem to be packed that day, becuase he looked like he had to use the bacthroom again lol!");
---
+++ Scripts/scripts.js
@@ -0,0 +1,17 @@
+let jobTitle="Student";
+let theLoction="Texas";
+let partnerName="Mattie";
+let numberChildren="2";
+console.log( "I am a "+jobTitle+ " in " +theLoction+ " and married to" +partnerName+ " with " +numberChildren);
+
+let myName="Lemuel";
+let myEmail="guestWhat@gmail.com";
+let myPassword="Welcome12@";
+let myAge="30";
+let myCountry="United States";
+let mySalary="5K";
+console.log( "whats up my name is " +myName+ " i live in the " +myCountry+ " currently i am " +myAge+ " show " +myEmail+ myPassword+" if you want to drop " +mySalary+ " on me lol.");
+
+let pi=3.14; 
+let radius=3;
+console.log()
